37ec93dd96a762ad8da2a84faae3cb7c29e94f17
Moeed-Tahir <moeedtahir29@gmail.com>
2025-06-02 11:50:26 +0500

    add some change in route

--- routes/v1/authRoute.js
+++ routes/v1/authRoute.js
@@ -8,8 +8,8 @@ router.post('/signup/initiate', authController.initiateSignup);
 router.post('/signup/verify-signup-otp', jwtMiddleware, authController.verifySignupOtp);
 router.post('/signin/verify-signin-otp', authController.verifySigninOtp);
 router.post('/signin', authController.signin);
-router.post('/signup/personal-info', authController.savePersonalInfo);
-router.post('/signup/verify-identity',authController.initiateIdentityVerification);
+router.post('/signup/personal-info', jwtMiddleware, authController.savePersonalInfo);
+router.post('/signup/verify-identity', jwtMiddleware, authController.initiateIdentityVerification);
 router.get('/verification-success', authController.handleVerificationSuccess);
 
 module.exports = router;
