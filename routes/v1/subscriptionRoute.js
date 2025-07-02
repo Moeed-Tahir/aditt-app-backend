19213c3bf504bb28d57a31be951ddd89a0343d52
Moeed-Tahir <moeedtahir29@gmail.com>
2025-07-02 17:36:11 +0500

    add some changes

--- routes/v1/subscriptionRoute.js
+++ routes/v1/subscriptionRoute.js
@@ -7,5 +7,7 @@ router.post('/subscription/createPlan', subscriptionController.createPlan);
 router.post('/subscription/fetchAllPlans', subscriptionController.fetchAllPlans);
 router.post('/subscription/createCustomerAndSetupIntent', subscriptionController.createCustomerAndSetupIntent);
 router.post('/subscription/subscribeCustomer', subscriptionController.subscribeCustomer);
+router.post('/payouts/payout', subscriptionController.payout);
+router.post('/payouts/confirmPayout', subscriptionController.confirmPayout);
 
 module.exports = router;
